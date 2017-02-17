7b87001580f7a213afaa605604ba35139d4b570d
Zac Flamig <Zac@WeatherWary.com>
2017-02-17 11:09:10 -0600

    feat(win): fixed travis yml error

--- src/Simulator.h
+++ src/Simulator.h
@@ -73,7 +73,7 @@ private:
   TimeUnit *timeStep, *timeStepSR, *timeStepLR, *timeStepPrecip, *timeStepQPF, *timeStepPET, *timeStepTemp, *timeStepTempF;
   float precipConvert, qpfConvert, petConvert, timeStepHours, timeStepHoursLR;
   TimeVar currentTime, currentTimePrecip, currentTimeQPF, currentTimePET, currentTimeTemp, currentTimeTempF, beginTime, endTime, warmEndTime, beginLRTime;
-  DatedName *precipFile, *qpfFile, *petFile, *tempFile, *tempFFile, currentTimeText, currentTimeTextOutput, currentDayTextOutput;
+  DatedName *precipFile, *qpfFile, *petFile, *tempFile, *tempFFile, currentTimeText, currentTimeTextOutput;
   std::vector<float> currentFF, currentSF, currentQ, avgPrecip, avgPET, avgSWE, currentSWE, avgT, avgSM, avgFF, avgSF, currentDepth;
   std::vector<FloatGrid *> paramGrids, paramGridsRoute, paramGridsSnow, paramGridsInundation;
   bool hasQPF, hasTempF, wantsDA;
